6fe8c4c0f298af8c904c62db1468e2c589758a7e
Vitaliy Ponomarenko <glitch.coder.256@gmail.com>
2021-02-02 10:26:30 +0200

    На ветке master Изменения, которые будут включены в коммит: 	новый файл:    content/lesson-1.php 	изменено:      css/main_style.css 	изменено:      index.html
    Выполнение задания 4: вывод таблицы умножения.
    !!! Требует правки т.к. header скролится со всей страницей.

---
+++ content/lesson-1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Учебный проект</title>
+    <link rel="stylesheet" href="../css/main_style.css">
+</head>
+<body>
+    <header>
+        <h1>Учебный проект</h1>
+    </header>
+    <main>
+        <nav>
+            <ul>
+                <li><a class="disabled" href="#">Урок 1</a></li>
+                <li><a href="#">Урок 2</a></li>
+            </ul>
+        </nav>
+        <h2>Таблица умножения</h2>
+        <table>
+            <?php
+
+            //применил такой подход чтобы не проверять каждый раз после выхода из второго for переход на
+            //следующую строку в таблице. В результате принесено в жертву два вызова функции.
+            function printRow()
+            {
+                //строка таблицы начинается с множителя
+                static $beginRow_multiplier1 = 1;
+
+                //строка таблицы заканчивается множителем
+                static $endRow_multiplier1 = 6;
+
+                echo "<tr>\n";
+
+                //$multiplier1 - множимое
+                for ($multiplier1 = $beginRow_multiplier1; $multiplier1 < $endRow_multiplier1; ++$multiplier1) {
+                    echo "<td>";
+
+                    //$multiplier2 - множитель
+                    for ($multiplier2 = 1; $multiplier2 < 11; ++$multiplier2) {
+                        echo "{$multiplier1} x {$multiplier2} = " . ($multiplier1 * $multiplier2) . "<br />";
+                    }
+
+                    echo "</td>\n";
+                }
+
+                echo "</tr>\n";
+
+                $beginRow_multiplier1 = 6;
+                $endRow_multiplier1 = 11;
+            }
+
+            printRow(); //распечатать строку с множимым от 1 до 5
+            printRow(); //распечатать строку с множимым от 6 до 10
+            ?>
+        </table>
+    </main>
+    <footer>
+        mailman&reg; Все права защищены
+    </footer>
+</body>
+</html>
